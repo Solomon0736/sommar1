f0e0bc20fe017c05e664ac37c6951805f12166fb
solomontikabo.musie <Nordmaling10>
2020-07-23 14:43:15 +0200

    Initial commit

---
+++ src/date.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class date {
+    public static void main(String[] args) {
+
+        System.out.println(" what is your name");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        System.out.println("hej" + " " + name);
+    }
+}
---
+++ src/test1.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class test1{
+    public static void main (String[] args) {
+        date seli= new date();
+
+      /*  int number []= new int[3];
+        number[0]=1;
+        number[2] =4;
+        System.out.println(Arrays.toString(number));
+       /* Point point1 = new Point(1,2);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point1 );*/
+      // räknar bokstaver i string ; indexOf vissar i vilket plats står det //
+      // String message = " hello\"world";
+        //System.out.println(message);
+        /*
+        den räknar hur många antalet av siffror finns det i den
+        int numbers[] = {1,2,3,2,4};
+        System.out.println( numbers.length);*/
+    }
+}
